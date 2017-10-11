835ca3a48eff2706dabc2faeec04aadfc69af0ea
Ryan LaBarge <ryanclabarge@gmail.com>
2017-10-10 20:10:28 -0400

    edited things

--- UseMortgageCalcs.java
+++ UseMortgageCalcs.java
@@ -13,10 +13,12 @@ public class UseMortgageCalcs{
 				"Please select a number:");
 	}
 
+
 	public static void main(String[] args){
 		NumberFormat formatter = NumberFormat.getCurrencyInstance();
 		Scanner scan = new Scanner(System.in);
-		double princiapl,term,rate;
+		double principal,term,rate;
+		int s = 0;
 		do{
 		System.out.println("Enter the total amount of the loan:");
 		principal = scan.nextDouble();
@@ -31,22 +33,51 @@ public class UseMortgageCalcs{
 		System.out.println("Enter the interest rate:");
 		rate = scan.nextDouble();
 		}
-		while(rate<1 || rate>15);
+		while(rate<0 || rate>15);
 		MortgageCalcs mort = new MortgageCalcs(rate,term,principal);
 		menu();
 		int t = scan.nextInt();
-		switch(t){
-			case 1: System.out.printf("%n%s:%s%n","Payment".formatter.format(mort.calcPayment()));
-				menu();
-				t = scan.nextInt();
-			case 2: System.out.printf("%n%s:%s%n","Future Value",formatter.format(mort.futureValue()));
-				menu();
-				t = scan.nextInt();
-			case 3: System.out.printf("%n%s:%s%n","Interest Charge:",formatter.format(mort.intCharge()));
-				menu();
-				t = scan.nextInt();
-			case 4: 
-			default: break;
-		} 
+		while(0<t && t<=6){
+			switch(t){
+				case 1:
+					System.out.printf("%n%s:%s%n","Payment",formatter.format(mort.calcPayment()));
+					menu();
+					t = scan.nextInt();
+					break;
+			
+				case 2:
+			       		System.out.printf("%n%s:%s%n","Future Value",formatter.format(mort.futureValue()));
+					menu();
+					t = scan.nextInt();
+					break;
+			
+				case 3:
+					System.out.printf("%n%s:%s%n","Interest Charge:",formatter.format(mort.intCharge()));
+					menu();
+					t = scan.nextInt();
+					break;
+			
+				case 4:
+					System.out.printf("%n%s:%s","Payment",formatter.format(mort.calcPayment()));
+					System.out.printf("%n%s:%s","Interest Charge:",formatter.format(mort.intCharge()));
+					System.out.printf("%n%s:%s%n","Payment",formatter.format(mort.calcPayment()));
+					menu();
+					t = scan.nextInt();
+					break;
+			
+				case 5: do{
+					System.out.printf("%s","How many months?");
+					s = scan.nextInt();
+					}	
+					while(1>s||s>12*term);
+					mort.amortize(s);
+					menu();
+					t = scan.nextInt();
+				       	break;	
+				default:
+				       	t = 7;
+					break;
+			}
+		}
 	}
 }
